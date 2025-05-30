4ed6288a14d1035b742bc09952d36099ead4c4a7
yashjaiswal5859 <sanabhagat@gmail.com>
2025-05-30 17:37:23 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const authRoutes = require('../routes/auth');
-const emailRoutes = require('../routes/email');
-const productRoutes = require('../routes/productDB');
-const getproductRoutes = require('../routes/scrap');
+const authRoutes = require('./routes/auth');
+const emailRoutes = require('./routes/email');
+const productRoutes = require('./routes/productDB');
+const getproductRoutes = require('./routes/scrap');
 const cheerio = require('cheerio');
 const axios = require('axios');
 
